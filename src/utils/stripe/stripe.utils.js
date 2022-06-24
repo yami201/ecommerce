5716039eb50589e18d34e3e36efb299f38f36116
yami201 <mohamednj2013@gmail.com>
2022-06-24 19:16:56 +0100

    starting with optimization

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,2 +1,2 @@
 import { loadStripe } from "@stripe/stripe-js"
-export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY)
+export const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`)
